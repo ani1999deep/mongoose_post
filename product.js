7c032248134795d457adf6b43c1d93ecb32da52a
ani1999deep <anideepbhowmick1@gmail.com>
2023-05-03 17:09:01 +0530

    first commit

---
+++ product.js
@@ -0,0 +1,11 @@
+//Define Schema and model
+const mongoose=require('mongoose');
+const productSchema=new mongoose.Schema({
+
+    name:String,
+    price:Number,
+    brand:String,
+    category:String
+});
+
+module.exports=mongoose.model('products',productSchema)
